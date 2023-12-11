6741b46b57b6ea38396e19ee79835a10a9c4d7e3
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-11 21:11:45 +0100

    Implement Logger

--- src/utils/logger.class.ts
+++ src/utils/logger.class.ts
@@ -1,1 +1,48 @@
-class Logger {}
+import { Logger as WinstonLogger, format, createLogger, transports } from 'winston';
+const { combine, timestamp, printf, colorize, prettyPrint, simple } = format;
+
+export class Logger {
+    private logger: WinstonLogger;
+
+    constructor() {
+        this.initialize();
+    }
+
+    debug(message: string, ...meta: any[]): void {
+        this.logger.debug(message, ...meta);
+    }
+
+    info(message: string, ...meta: any[]): void {
+        this.logger.info(message, ...meta);
+    }
+
+    error(message: string, ...meta: any[]): void {
+        this.logger.error(message, ...meta);
+    }
+
+    private initialize(): void {
+        this.logger = createLogger({
+            level: 'debug',
+            format: combine(
+                timestamp({
+                    format: 'YYYY-MM-DD HH:MM:SS',
+                }),
+                prettyPrint(),
+                format.splat(),
+                simple(),
+                printf((msg) => {
+                    const message = msg.message;
+                    const splat = msg[Symbol.for('splat')];
+
+                    return colorize().colorize(
+                        msg.level,
+                        `[${msg.timestamp}] [${msg.level.toLocaleUpperCase()}] - ${message}${
+                            splat ? ' ' + JSON.stringify(splat) : ''
+                        }`,
+                    );
+                }),
+            ),
+            transports: [new transports.Console()],
+        });
+    }
+}
