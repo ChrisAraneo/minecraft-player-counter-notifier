0af9d976ceb1015f06db30111887d930e45af815
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-13 23:02:20 +0100

    Implement API client #getNumberOfOnlinePlayers, create response type

--- src/api/mc-api-client.class.ts
+++ src/api/mc-api-client.class.ts
@@ -1,3 +1,31 @@
-class McApiClient {
-    constructor() {}
+import fetch from 'node-fetch';
+import { StatusResponse } from './status-response.type';
+import { Logger } from '../utils/logger.class';
+
+export class McApiClient {
+    private static readonly CacheTTL: number = 30000;
+
+    private static StatusEndpoint = `https://api.mcsrvstat.us/3`;
+    private static Cache = new Map<string, { timestamp: Date; response: StatusResponse }>();
+
+    constructor(private logger: Logger) {}
+
+    async getNumberOfOnlinePlayers(server: string, now: Date = new Date()): Promise<number | void> {
+        const cached = McApiClient.Cache.get(server);
+
+        if (cached?.timestamp && +cached.timestamp + McApiClient.CacheTTL < +now) {
+            return cached.response.players.online;
+        }
+
+        return await fetch(`${McApiClient.StatusEndpoint}/${server}`)
+            .then((response) => response.json() as unknown as StatusResponse)
+            .then((json: StatusResponse) => {
+                McApiClient.Cache.set(server, { timestamp: now, response: json });
+
+                return json.players.online;
+            })
+            .catch((error) => {
+                this.logger.error(error);
+            });
+    }
 }
---
+++ src/api/status-response.type.ts
@@ -0,0 +1,44 @@
+export type StatusResponse = {
+    ip: string;
+    port: number;
+    debug: {
+        ping: boolean;
+        query: boolean;
+        srv: boolean;
+        querymismatch: boolean;
+        ipinsrv: boolean;
+        cnameinsrv: boolean;
+        animatedmotd: boolean;
+        cachehit: boolean;
+        cachetime: number;
+        cacheexpire: number;
+        apiversion: number;
+        dns: {
+            srv: any[];
+            srv_a: any[];
+        };
+    };
+    motd: {
+        raw: string[];
+        clean: string[];
+        html: string[];
+    };
+    players: {
+        online: number;
+        max: number;
+    };
+    version: string;
+    online: boolean;
+    protocol: {
+        version: number;
+        name: string;
+    };
+    hostname: string;
+    icon: string;
+    map: {
+        raw: string;
+        clean: string;
+        html: string;
+    };
+    eula_blocked: boolean;
+};
