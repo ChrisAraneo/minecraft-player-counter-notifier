0458266375e79547f374d22e3d27547e4a8efb2d
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-16 18:36:06 +0100

    Reformat code

--- src/api/minecraft-server-status-api-client.class.ts
+++ src/api/minecraft-server-status-api-client.class.ts
@@ -1,6 +1,5 @@
 import fetch from 'node-fetch';
 import { StatusResponse } from './status-response.type';
-import { Logger } from '../utils/logger.class';
 
 export class MinecraftServerStatusApiClient {
     private static readonly CacheTTL: number = 30000;
@@ -8,24 +7,28 @@ export class MinecraftServerStatusApiClient {
     private static StatusEndpoint = `https://api.mcsrvstat.us/3`;
     private static Cache = new Map<string, { timestamp: Date; response: StatusResponse }>();
 
-    constructor(private logger: Logger) {}
+    constructor() {}
 
+    // TODO Use observables
     async getNumberOfOnlinePlayers(server: string, now: Date = new Date()): Promise<number | void> {
         const cached = MinecraftServerStatusApiClient.Cache.get(server);
 
-        if (cached?.timestamp && +cached.timestamp + MinecraftServerStatusApiClient.CacheTTL < +now) {
+        if (
+            cached?.timestamp &&
+            +cached.timestamp + MinecraftServerStatusApiClient.CacheTTL < +now
+        ) {
             return cached.response.players.online;
         }
 
         return await fetch(`${MinecraftServerStatusApiClient.StatusEndpoint}/${server}`)
             .then((response) => response.json() as unknown as StatusResponse)
             .then((json: StatusResponse) => {
-                MinecraftServerStatusApiClient.Cache.set(server, { timestamp: now, response: json });
+                MinecraftServerStatusApiClient.Cache.set(server, {
+                    timestamp: now,
+                    response: json,
+                });
 
                 return json.players.online;
-            })
-            .catch((error) => {
-                this.logger.error(error);
             });
     }
 }
