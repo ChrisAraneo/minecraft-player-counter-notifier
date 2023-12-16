06398da7383e955eba63793db6fa0185c0902f9b
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-16 18:37:50 +0100

    Update main function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
+import { MinecraftServerStatusApiClient } from './api/minecraft-server-status-api-client.class';
+import { ConfigLoader } from './file-system/config-loader.class';
+import { CurrentDirectoryProvider } from './file-system/current-directory-provider.class';
 import { Logger } from './utils/logger.class';
+import { FileSystem } from './file-system/file-system.class';
+import { EMPTY, catchError, firstValueFrom } from 'rxjs';
 
-const logger: Logger = new Logger();
+(async () => {
+    const logger: Logger = new Logger();
 
-logger.info('Minecraft Player Number Notifier v0.01');
+    logger.info('Minecraft Player Number Notifier v0.01');
+
+    const currentDirectoryProvider = new CurrentDirectoryProvider();
+    const fileSystem = new FileSystem();
+    const configLoader = new ConfigLoader(currentDirectoryProvider, fileSystem);
+
+    const config: any = await firstValueFrom(
+        configLoader.readConfigFile().pipe(
+            catchError((error) => {
+                logger.error(error);
+
+                return EMPTY;
+            }),
+        ),
+    );
+
+    const apiClient = new MinecraftServerStatusApiClient();
+    (config.servers as string[]).forEach(async (server) => {
+        const numberOfPlayers = await apiClient.getNumberOfOnlinePlayers(server);
+
+        logger.info(`Server ${server} has currently: ${numberOfPlayers} players.`);
+    });
+})();
