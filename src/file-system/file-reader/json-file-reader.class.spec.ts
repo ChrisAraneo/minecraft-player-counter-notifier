619fa81a30948f44f9bd0229282f7ec040121202
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-29 21:12:33 +0100

    Add basic JsonFileReader unit tests

---
+++ src/file-system/file-reader/json-file-reader.class.spec.ts
@@ -0,0 +1,34 @@
+import { lastValueFrom } from 'rxjs';
+import { FileSystem } from '../file-system/file-system.class';
+import { FileSystemMock } from '../file-system/file-system.mock.class';
+import { JsonFileReader } from './json-file-reader.class';
+
+let fileSystem: FileSystem;
+let reader: JsonFileReader;
+
+beforeEach(() => {
+    fileSystem = new FileSystemMock();
+    reader = new JsonFileReader(fileSystem);
+});
+
+describe('JsonFileReader', () => {
+    it('#readFile should read text file', async () => {
+        jest.spyOn(fileSystem, 'readFile');
+
+        await lastValueFrom(reader.readFile('test.json'));
+
+        const call = jest.mocked(fileSystem.readFile).mock.calls[0];
+        expect(call[0]).toBe('test.json');
+        expect(call[1]).toBe('utf-8');
+        expect(typeof call[2]).toBe('function');
+    });
+
+    it('#readFiles should read multiple text files', async () => {
+        jest.spyOn(fileSystem, 'readFile');
+
+        await lastValueFrom(reader.readFiles(['test.json', 'test2.json', 'test3.json']));
+
+        const calls = jest.mocked(fileSystem.readFile).mock.calls;
+        expect(calls.length).toBe(3);
+    });
+});
