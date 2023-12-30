136759c1eedeeb68145a2051007ec57ca4d30061
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-30 19:40:59 +0100

    Implement ProcessMock

---
+++ src/process/process.mock.class.ts
@@ -0,0 +1,13 @@
+import { DISCORD_TOKEN, RECIPIENTS } from './argument-keys.consts';
+import { Process } from './process.class';
+
+export class ProcessMock extends Process {
+    get argv(): string[] {
+        return [
+            'C:\\nodejs\\node.exe',
+            'C:minecraft-players-number-notifier\\dist\\mpnn.js',
+            `${DISCORD_TOKEN}=01234543210`,
+            `${RECIPIENTS}=rec1;rec2;rec3`,
+        ];
+    }
+}
