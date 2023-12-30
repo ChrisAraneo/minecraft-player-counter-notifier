e78f93e026127779802b0c4c2eecbbf759f7dd49
Krzysztof Pająk <chris.araneo@gmail.com>
2023-12-30 18:38:25 +0100

    Update error handling

--- src/file-system/file-reader/json-file-reader.class.ts
+++ src/file-system/file-reader/json-file-reader.class.ts
@@ -1,15 +1,23 @@
 import { Observable, map } from 'rxjs';
 import { JsonFile } from '../../models/json-file.class';
 import { FileReader } from './file-reader.class';
+import { JSON_FILE_PARSING_ERROR_MESSAGE } from './json-file-reader.consts';
 import { ReadFileResult } from './read-file-result.type';
 
 export class JsonFileReader extends FileReader<JsonFile> {
     readFile(path: string): Observable<JsonFile> {
         return this._readFile(path, 'utf-8').pipe(
-            map(
-                (result: ReadFileResult) =>
-                    new JsonFile(result.path, JSON.parse(result.data), result.modifiedDate),
-            ),
+            map((result: ReadFileResult) => {
+                let content: object | undefined;
+
+                try {
+                    content = JSON.parse(result.data);
+                } catch (error: unknown) {
+                    throw Error(JSON_FILE_PARSING_ERROR_MESSAGE);
+                }
+
+                return new JsonFile(result.path, content, result.modifiedDate);
+            }),
         );
     }
 }
---
+++ src/file-system/file-reader/json-file-reader.consts.ts
@@ -0,0 +1,1 @@
+export const JSON_FILE_PARSING_ERROR_MESSAGE = 'Could not parse json file';
