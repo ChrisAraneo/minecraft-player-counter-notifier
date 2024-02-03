bad87438e921eb965ce182ac73538f3460dd41cf
Krzysztof Pająk <chris.araneo@gmail.com>
2024-02-03 17:10:21 +0100

    Add unit tests

---
+++ src/api/discord-api-message.class.spec.ts
@@ -0,0 +1,66 @@
+import { Player } from '../models/player.type';
+import { DiscordApiMessage } from './discord-api-message.class';
+
+describe('DiscordApiMessage', () => {
+    let recipientId: string;
+    let server: string;
+    let numberOfPlayers: number;
+    let playersList: Player[];
+
+    beforeEach(() => {
+        recipientId = '5468';
+        server = 'example.com';
+        numberOfPlayers = 2;
+        playersList = [
+            {
+                uuid: '6f9ca9ab-8f38-4cd8-a858-f8f2b950598a',
+                name: 'John',
+            },
+            {
+                uuid: '5a755c70-c39a-4811-a259-4e5aca7bdea7',
+                name: 'Adam',
+            },
+        ];
+    });
+
+    describe('getId', () => {
+        it('should return id', async () => {
+            const message = new DiscordApiMessage(
+                recipientId,
+                server,
+                numberOfPlayers,
+                playersList,
+            );
+
+            expect(message.getId()).toEqual('c615b6276813e57a0f463b75b44407ec');
+        });
+    });
+
+    describe('getRecipientId', () => {
+        it('should return recipient id', async () => {
+            const message = new DiscordApiMessage(
+                recipientId,
+                server,
+                numberOfPlayers,
+                playersList,
+            );
+
+            expect(message.getRecipientId()).toEqual(recipientId);
+        });
+    });
+
+    describe('getMessage', () => {
+        it('should return message content', async () => {
+            const message = new DiscordApiMessage(
+                recipientId,
+                server,
+                numberOfPlayers,
+                playersList,
+            );
+
+            expect(message.getMessage()).toEqual(
+                `2 players ${String.fromCodePoint(0x1f6b6)} on server example.com: John, Adam`,
+            );
+        });
+    });
+});
